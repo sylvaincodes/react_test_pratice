6cca4a543152974e3f36df07cf47ce38533dccfe
sylvaincodes <sylvaincodeur@gmail.com>
2024-01-20 15:37:23 +0100

    Set up example test for Jest and React testing library

--- App.js
+++ App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import Header from "./src/Header";
+import Footer from "./src/Footer";
 
-const App = () => {
-  return <div>App</div>;
-};
+function App() {
+  const [action, setAction] = useState("action");
+  const handleClick = () => {
+    setAction("clicked");
+  };
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="container">
+          <div className="row">
+            <h1>App</h1>
+            <button type="button" onClick={handleClick}>
+              {action}
+            </button>
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+}
 
 export default App;
--- tests/App.test.js
+++ tests/App.test.js
@@ -1,6 +1,50 @@
-import renderer from "react-test-renderer";
+import { render, screen, fireEvent } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+import ReactDOM from "react-dom/client";
 import App from "../App";
 
-test("afficher le composant App", () => {
-  renderer.create(<App />);
+describe("App", () => {
+
+
+  let container;
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    document.body.removeChild(container);
+    container = null;
+  });
+
+  it("should render without crash", () => {
+    // Arrange
+    act(() => {
+      ReactDOM.createRoot(container).render(<App />);
+    });
+  });
+
+  it("should get h1 content", () => {
+    // Arrange
+    act(() => {
+      ReactDOM.createRoot(container).render(<App />);
+    });
+
+    // Assertion
+    const title = container.querySelector("h1");
+    expect(title.textContent).toBe("App");
+  });
+
+  // we use  React Testing Library utils.
+  it("should click the button", () => {
+    // Arrange
+    render(<App />); // on utilise la fonction render de react testing qui contient deja act
+
+    //ACT
+    const button = screen.getByRole("button"); // recupere l'element button avec screen (body)
+    fireEvent.click(button);
+
+    //Assertion : une bout de code qui teste si une logique est vraie ou fausse
+    expect(button.textContent).toBe("clicked");
+  });
 });
